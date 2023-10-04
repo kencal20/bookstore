d437c257d1d80a9d8c8662ef93c679b6535c4d27
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-10-04 11:49:10 +0000

    adding icons to the header 1 component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,23 +1,33 @@
 import React from "react";
-import './header.css'
+import "./header.css";
 export default function Header() {
   return (
     <div>
       <header className="header">
         <div className="header-1">
-          <a href="asdas" class="logo">
-            {" "}
+          <a href="asdas" className="logo">
             <i className="fas fa-book"></i>brooklyn
           </a>
           <form action="" className="search-form">
             <input
               type="search"
-              name=""
-              placeholder="search heare....."
+              placeholder="search here....."
               id="search-box"
             />
             <label for="search-box" className="fas fa-search"></label>
           </form>
+          <div className="icons">
+            <div id="search-btn" className="fas fa-search"></div>
+            <a href="like">
+              <i className="fas fa-heart"></i>
+            </a>
+            <a href="cart">
+              <i className="fas fa-shopping-cart"></i>
+            </a>
+            <a href="user">
+              <i className="fas fa-user"></i>
+            </a>
+          </div>
         </div>
       </header>
     </div>
